f47854b3d97f6c72e0552a1936cb811184c612f8
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2024-02-27 17:20:52 -0500

    feat: rename the mouse labels
    Signed-off-by: Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>

--- creedenzymatic_analysis.R
+++ creedenzymatic_analysis.R
@@ -3,41 +3,37 @@
 library(tidyverse)
 library(creedenzymatic)
 
-process_creedenzymatic <-
-  function(krsa_path, uka_path, peptide_path) {
-    krsa_data <- read_csv(krsa_path, show_col_types = FALSE) |>
-      select(Kinase, Score = AvgZ) |>
-      read_krsa(trns = "abs", sort = "desc")
+process_creedenzymatic <- function(krsa_path, uka_path, peptide_path) {
+  krsa_data <- read_csv(krsa_path, show_col_types = FALSE) |>
+    select(Kinase, Score = AvgZ) |>
+    read_krsa(trns = "abs", sort = "desc")
 
-    uka_data <- read_tsv(uka_path, show_col_types = FALSE) |>
-      select(Kinase = `Kinase Name`, Score = `Median Final score`) |>
-      read_uka(trns = "abs", sort = "desc")
+  uka_data <- read_tsv(uka_path, show_col_types = FALSE) |>
+    select(Kinase = `Kinase Name`, Score = `Median Final score`) |>
+    read_uka(trns = "abs", sort = "desc")
 
-    peptide_data <-
-      read_csv(peptide_path, show_col_types = FALSE) |>
-      select(Peptide, Score = totalMeanLFC)
+  peptide_data <- read_csv(peptide_path, show_col_types = FALSE) |>
+    select(Peptide, Score = totalMeanLFC)
 
-    kea3_data <-
-      read_kea(
-        peptide_data,
-        sort = "asc",
-        trns = "abs",
-        method = "MeanRank",
-        lib = "kinase-substrate"
-      )
+  kea3_data <- read_kea(
+    peptide_data,
+    sort = "asc",
+    trns = "abs",
+    method = "MeanRank",
+    lib = "kinase-substrate"
+  )
 
-    ptmsea_data <-
-      read_ptmsea(peptide_data)
+  ptmsea_data <- read_ptmsea(peptide_data)
 
-    combined <- combine_tools(
-      KRSA_df = krsa_data,
-      UKA_df = uka_data,
-      KEA3_df = kea3_data,
-      PTM_SEA_df = ptmsea_data
-    )
+  combined <- combine_tools(
+    KRSA_df = krsa_data,
+    UKA_df = uka_data,
+    KEA3_df = kea3_data,
+    PTM_SEA_df = ptmsea_data
+  )
 
-    combined
-  }
+  combined
+}
 
 krsa_files <- c(
   "results//krsa-krsa_table_full_CMS_CTL_STK.csv",
@@ -46,9 +42,9 @@ krsa_files <- c(
 )
 
 uka_files <- c(
-  "kinome_data/UKA/CMS-CTL/Summaryresults 20231024-1011.txt",
-  "kinome_data/UKA/CPP-CTL/Summaryresults 20231024-1014.txt",
-  "kinome_data/UKA/CTL-SPP/Summaryresults 20231024-1016.txt"
+  "kinome_data/UKA/SN-CN/Summaryresults 20231024-1011.txt",
+  "kinome_data/UKA/CBT-CN/Summaryresults 20231024-1014.txt",
+  "kinome_data/UKA/CN-SBT/Summaryresults 20231024-1016.txt"
 )
 
 peptide_files <- c(
@@ -64,5 +60,5 @@ result <-
     peptide_path = peptide_files
   ) |>
   pmap(process_creedenzymatic) |>
-  set_names(c("CMS-CTL", "CPP-CTL", "SPP-CTL")) |>
+  set_names(c("SN-CN", "CBT-CN", "SBT-CN")) |>
   imap_dfr(~ write_csv(.x, str_glue("results/{.y}_creedenzymatic.csv")), .id = "Comparison")
